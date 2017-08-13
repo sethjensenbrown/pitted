dad0614eaec17b5d4973c0508ced263b0ea37243
sethjensenbrown <sethjensenbrown@gmail.com>
2017-08-13 13:30:16 -0700

    herkou fix

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
-/*app.get('/', (req,res) => {
-	res.status(200);
-})*/
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+  res.status(200);
+});
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
 
 module.exports = {app};
