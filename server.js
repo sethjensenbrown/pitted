1bfb2e2599513777d01955d2aa79ee811f9678d6
sethjensenbrown <sethjensenbrown@gmail.com>
2017-08-13 12:20:03 -0700

    continuing setup

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+app.get('/', (req,res) => {
+	res.status(200);
+})
+
+
 app.listen(process.env.PORT || 8080);
+
+module.exports = {app};
---
+++ test/test-server.js
@@ -0,0 +1,19 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const {app} = require('../server');
+chai.use(chaiHttp);
+
+
+
+describe('server', function() {
+
+	it('should return 200', function() {
+		return chai.request(app)
+				.get('/')
+				.then(function(res) {
+					res.should.have.status(200);
+				})
+	})
+
+})
