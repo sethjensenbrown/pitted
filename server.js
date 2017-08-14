fe7310d8d1dd32f3dcad0ac7451217f6df9a51a0
sethjensenbrown <sethjensenbrown@gmail.com>
2017-08-13 20:50:41 -0700

    changed port definition based on arron's code

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 app.use(express.static('public'));
+const port = process.env.PORT || 8080; 
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + '/index.html');
   res.status(200);
 });
 
-app.listen(process.env.PORT || 8080, () => {
-	console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+app.listen(port);
+//app.set('port', (process.env.PORT || 3000)); 
+
+/*app.listen(app.get('port'), function(){
+	console.log('Server started on port '+app.get('port')); 
+});*/
 
 module.exports = {app};
