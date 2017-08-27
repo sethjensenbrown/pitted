067be81ac2688758632a044f16a0903c3e90da17
sethjensenbrown <sethjensenbrown@gmail.com>
2017-08-27 10:50:12 -0700

    added endpoint and functionality for state search

--- public/home.js
+++ public/home.js
@@ -1,3 +1,5 @@
+var SURF_SPOTS;
+
 //gets state from form
 var getState = () => {
 	return $('#search-state option:checked').val();
@@ -11,6 +13,84 @@ var getZip = () => {
 //gets radius from form
 var getRadius = () => {
 	return $('#search-radius option:checked').val();
+}
+
+//creates array that holds spot elements
+var makeTemplateArray = (data) => {
+	return data.map((spot) => {
+		//spot elements based on template in admin-menu.html file
+		var template = $('#spot-template').clone();
+		//original template is hidden
+		template.removeClass('hidden');
+		//replaces template values with spot values
+		template.find('.js-template-name').html(spot.name);
+		template.find('.js-template-img').attr("src", spot.image_url);
+		template.find('.js-template-username').html(spot["admin-id"]);
+		template.find('.js-template-difficulty').html(spot.difficulty);
+		template.find('.js-template-state').html(spot.location.state);
+		template.find('.js-template-lad').html(spot.location.laditude);
+		template.find('.js-template-lng').html(spot.location.longitude);
+		//stores new spot element in template_array
+		return template;
+	})
+};
+
+//adds each spot element to DOM
+var addHTML = (templateArray) => {
+	templateArray.forEach((template) => {
+		$('#spots-container').append(template);
+	})
+};
+
+//creates a map using Google Map API
+var initMap = (spots) => {
+	var map;
+    var bounds = new google.maps.LatLngBounds();
+    var mapOptions = {
+    	center: {lat: 38, lng: -19},
+  		zoom: 3,
+        mapTypeId: 'roadmap'
+    };
+                    
+    //displays a map on the page
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+    map.setTilt(45);
+        
+    //displays multiple markers on the map
+    var infoWindow = new google.maps.InfoWindow(), marker, i;
+    
+    //loops through the array of spots & places each one on the map  
+    for( i = 0; i < spots.length; i++ ) {
+        var position = new google.maps.LatLng(
+        	spots[i].location.laditude, spots[i].location.longitude);
+        bounds.extend(position);
+        marker = new google.maps.Marker({
+            position: position,
+            map: map,
+            title: spots[i][name]
+        });
+        
+        //creates an info window for each marker   
+        google.maps.event.addListener(marker, 'click', (function(marker, i) {
+            return function() {
+                infoWindow.setContent(
+                	`<h5>${spots[i].name}</h5>
+                	 <p>This spot is ${spots[i].difficulty}</p>`
+                	);
+                infoWindow.open(map, marker);
+            }
+        })(marker, i));
+
+        //automatically centers the map fitting all markers on the screen
+        map.fitBounds(bounds);
+    }
+}
+
+var displayResults = (results) => {
+	$('#search-container').addClass('hidden');
+	initMap(results);
+	addHTML(makeTemplateArray(results));
+	$('#results-container').removeClass('hidden');
 }
 
 //event listener for search button
@@ -38,5 +118,8 @@ $('#search-button').on('click', (event) => {
 	}
 	else {
 		console.log(`Find all spots in ${getState()}`);
+		$.getJSON(window.location.href + 'results/state/' + getState(), (results) => {
+			displayResults(results);
+		});
 	}
 })
