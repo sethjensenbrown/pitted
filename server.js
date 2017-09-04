876ee872d1e7f68de3040ca9a0f693a0b7d614ca
sethjensenbrown <sethjensenbrown@gmail.com>
2017-09-03 17:39:52 -0700

    added morgan, passport, and CORS to server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const passport = reuire('passport');
 
 //forces mongoose to use es6 promises instead of it's own
 mongoose.Promise = global.Promise;
@@ -11,6 +13,18 @@ const {SurfSpots} = require('./models');
 const app = express();
 app.use(express.static('public'));
 app.use(bodyParser.json());
+app.use(morgan('common'));
+
+// CORS
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+  if (req.method === 'OPTIONS') {
+    return res.send(204);
+  }
+  next();
+});
 
 /***********************************************************/
 
