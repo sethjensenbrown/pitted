b7c562d2e27d9600bb73a613fc0610dc02e5edcc
sethjensenbrown <sethjensenbrown@gmail.com>
2017-09-20 19:04:18 -0700

    removed CORS

--- server.js
+++ server.js
@@ -26,16 +26,6 @@ app.use(passport.initialize());
 passport.use(basicStrategy);
 passport.use(jwtStrategy);
 
-// CORS
-/*app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
-});*/
 /***********************************************************/
 
 //STATIC PAGES ENDPOINTS
